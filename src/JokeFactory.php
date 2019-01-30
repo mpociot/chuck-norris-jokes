38d8d67a7a5f090b0870fd14c5afbd12e9808f88
Marcel Pociot <m.pociot@gmail.com>
2019-01-30 15:51:36 +0100

    Initial commit

---
+++ src/JokeFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Mpociot\ChuckNorrisJokes;
+
+class JokeFactory
+{
+	protected $jokes = [
+		'Chuck Norris\' tears cure cancer. Too bad he has never cried.',
+		'Chuck Norris counted to infinity... Twice.',
+		'If you can see Chuck Norris, he can see you. If you can\'t see Chuck Norris you may be only seconds away from death.'
+	];
+
+	public function __construct(array $jokes = null)
+	{
+		if ($jokes) {
+			$this->jokes = $jokes;
+		}
+	}
+
+	public function getRandomJoke()
+	{
+		return $this->jokes[array_rand($this->jokes)];
+	}
+}
---
+++ tests/JokeFactoryTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Mpociot\ChuckNorrisJokes\Tests;
+
+use Mpociot\ChuckNorrisJokes\JokeFactory;
+use PHPUnit\Framework\TestCase;
+
+class JokeFactoryTest extends TestCase
+{
+	/** @test */
+	public function it_returns_a_random_joke()
+	{
+		$jokes = new JokeFactory([
+			'This is a joke',
+		]);
+
+		$joke = $jokes->getRandomJoke();
+
+		$this->assertSame('This is a joke', $joke);
+	}
+
+	/** @test */
+	public function it_returns_a_predefined_joke()
+	{
+		$chuckNorrisJokes = [
+				'Chuck Norris\' tears cure cancer. Too bad he has never cried.',
+				'Chuck Norris counted to infinity... Twice.',
+				'If you can see Chuck Norris, he can see you. If you can\'t see Chuck Norris you may be only seconds away from death.'
+			];
+
+		$jokes = new JokeFactory();
+
+		$joke = $jokes->getRandomJoke();
+
+		$this->assertContains($joke, $chuckNorrisJokes);
+	}
+}
