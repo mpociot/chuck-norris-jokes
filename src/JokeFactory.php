340c3cf037feac72602a37b2b213e5e017e46451
Marcel Pociot <m.pociot@gmail.com>
2019-02-04 12:19:39 +0100

    Apply fixes from StyleCI (#3)

--- src/JokeFactory.php
+++ src/JokeFactory.php
@@ -4,21 +4,21 @@ namespace Mpociot\ChuckNorrisJokes;
 
 class JokeFactory
 {
-	protected $jokes = [
-		'Chuck Norris\' tears cure cancer. Too bad he has never cried.',
-		'Chuck Norris counted to infinity... Twice.',
-		'If you can see Chuck Norris, he can see you. If you can\'t see Chuck Norris you may be only seconds away from death.'
-	];
+    protected $jokes = [
+        'Chuck Norris\' tears cure cancer. Too bad he has never cried.',
+        'Chuck Norris counted to infinity... Twice.',
+        'If you can see Chuck Norris, he can see you. If you can\'t see Chuck Norris you may be only seconds away from death.',
+    ];
 
-	public function __construct(array $jokes = null)
-	{
-		if ($jokes) {
-			$this->jokes = $jokes;
-		}
-	}
+    public function __construct(array $jokes = null)
+    {
+        if ($jokes) {
+            $this->jokes = $jokes;
+        }
+    }
 
-	public function getRandomJoke()
-	{
-		return $this->jokes[array_rand($this->jokes)];
-	}
+    public function getRandomJoke()
+    {
+        return $this->jokes[array_rand($this->jokes)];
+    }
 }
