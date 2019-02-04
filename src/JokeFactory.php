4e0a05b2f31381253593014694d0e26bc8b06894
Freek Van der Herten <freek@spatie.be>
2019-02-04 22:33:49 +0100

    Add bad joke

--- src/JokeFactory.php
+++ src/JokeFactory.php
@@ -8,6 +8,7 @@ class JokeFactory
         'Chuck Norris\' tears cure cancer. Too bad he has never cried.',
         'Chuck Norris counted to infinity... Twice.',
         'If you can see Chuck Norris, he can see you. If you can\'t see Chuck Norris you may be only seconds away from death.',
+        'Chuck Norris has already been to Mars; that\'s why there are no signs of life there.',
     ];
 
     public function __construct(array $jokes = null)
