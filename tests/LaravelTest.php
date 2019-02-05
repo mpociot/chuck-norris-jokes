62bc55568691782c7a8d1212a02575be7888a4a5
Marcel Pociot <m.pociot@gmail.com>
2019-02-05 16:44:06 +0100

    Apply fixes from StyleCI (#7)

--- tests/LaravelTest.php
+++ tests/LaravelTest.php
@@ -2,41 +2,41 @@
 
 namespace Mpociot\ChuckNorrisJokes\Tests;
 
-use Illuminate\Support\Facades\Artisan;
-use Mpociot\ChuckNorrisJokes\ChuckNorrisJokesServiceProvider;
-use Mpociot\ChuckNorrisJokes\Console\ChuckNorrisJoke;
-use Mpociot\ChuckNorrisJokes\Facades\ChuckNorris;
 use Orchestra\Testbench\TestCase;
+use Illuminate\Support\Facades\Artisan;
+use Mpociot\ChuckNorrisJokes\Facades\ChuckNorris;
+use Mpociot\ChuckNorrisJokes\Console\ChuckNorrisJoke;
+use Mpociot\ChuckNorrisJokes\ChuckNorrisJokesServiceProvider;
 
 class LaravelTest extends TestCase
 {
-	protected function getPackageProviders($app)
-	{
-	    return [
-	    	ChuckNorrisJokesServiceProvider::class
-	    ];
-	}
+    protected function getPackageProviders($app)
+    {
+        return [
+            ChuckNorrisJokesServiceProvider::class,
+        ];
+    }
 
-	protected function getPackageAliases($app)
-	{
-	    return [
-	        'ChuckNorris' => ChuckNorrisJoke::class
-	    ];
-	}
+    protected function getPackageAliases($app)
+    {
+        return [
+            'ChuckNorris' => ChuckNorrisJoke::class,
+        ];
+    }
 
-	/** @test */
-	public function the_console_command_returns_a_joke()
-	{
-		$this->withoutMockingConsoleOutput();
+    /** @test */
+    public function the_console_command_returns_a_joke()
+    {
+        $this->withoutMockingConsoleOutput();
 
-		ChuckNorris::shouldReceive('getRandomJoke')
-			->once()
-			->andReturn('some joke');
+        ChuckNorris::shouldReceive('getRandomJoke')
+            ->once()
+            ->andReturn('some joke');
 
-		$this->artisan('chuck-norris');
+        $this->artisan('chuck-norris');
 
-		$output = Artisan::output();
+        $output = Artisan::output();
 
-		$this->assertSame('some joke'.PHP_EOL, $output);
-	}
+        $this->assertSame('some joke'.PHP_EOL, $output);
+    }
 }
